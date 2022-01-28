ad2c52efe169931691ddc39213f31605e56caab4
Alberto Septien <tavros@albertos-air.lan>
2022-01-28 13:47:10 -0800

    First commit homepage done

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,63 @@
+import { Container, makeStyles, Typography } from '@material-ui/core';
+import { findByLabelText } from '@testing-library/react';
+import React from 'react';
+import Carousel from './Carousel';
+
+const useStyles = makeStyles(() => ({
+    banner:{
+        backgroundImage: "url(./banner3.jpg)",
+        backgroundPosition: "center"
+    },
+    bannerContent: {
+        height: 300,
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: 25,
+        justifyContent: "space-around",
+        alignItems: "center"
+
+    },
+    tagline: {
+        display: "flex",
+        height: "40%",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+    },
+}));
+
+const Banner = () => {
+
+  const classes = useStyles()
+    
+  return <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+            <div className={classes.tagline}>
+                <Typography 
+                    variant='h3'
+                    style={{
+                        fontWeight: "bold",
+                        marginBottom: 15,
+                        fontFamily: "Montserrat",
+                    }}> 
+                        DARK ALLEY
+                </Typography>
+                <Typography 
+                    variant='subtitle2'
+                    style={{
+                        color: "white",
+                        fontWeight: "light",
+                        marginBottom: 15,
+                        fontFamily: "Montserrat",
+                    }}> 
+                        All of your degeneracies in one place
+                </Typography>
+            </div>
+            <Carousel/>
+      </Container>
+
+
+  </div>;
+};
+
+export default Banner;
---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { CryptoState } from '../CryptoContext';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: "magenta",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    }
+}))
+
+const Header = () => {
+
+    const classes = useStyles();
+
+    const history = useHistory();
+
+    const { currency, setCurrency } = CryptoState();
+
+    console.log(currency);
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
+
+    return( 
+        <ThemeProvider theme={darkTheme}>
+        <AppBar color='transparent' position='static'>
+            <Container>
+                <Toolbar>
+                    <Typography 
+                    onClick={() => history.push('/')}
+                    className={classes.title}
+                    variant='h4'
+                    style={{
+                        marginTop: 25,
+                        marginBottom: -15,
+                        fontFamily: "Montserrat",
+                    }}>
+                    Dark Alley
+                    </Typography>
+                </Toolbar>
+
+                <Select variant='outlined'
+                        style={{
+                            width: 100,
+                            height: 40,
+                            marginLeft: "80%",
+                            marginTop: -15,
+                }}
+                    value={currency}
+                    onChange={(e) => setCurrency(e.target.value)} >
+                    <MenuItem value={'USD'}>USD</MenuItem>
+                    <MenuItem value={"CAD"}>CAD</MenuItem>
+                </Select>
+
+            </Container>
+        </AppBar>
+        </ThemeProvider>
+    )
+};
+
+export default Header;
